d896c75054ba8eef1df7562b9b07737950fc5c13
Aditya <96467341+adityaprakash-dev@users.noreply.github.com>
2023-09-15 02:36:56 +0530

    Uses React, Next, and Tailwind

---
+++ page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import Header from './components/Header';
+import Todo from './components/Todo';
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <Todo />
+    </>
+  );
+};
+
+export default Page;
